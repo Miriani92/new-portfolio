b347dbf2268557bd581a3d217ff470dfe4ad68fd
miriani <mirian.tsintsadze1992@gmail.com>
2022-06-04 16:24:49 +0400

    navlinks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/" element={<App />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,37 @@
 import React from "react";
 import styles from "./Header.module.css";
+import { NavLink } from "react-router-dom";
+import myImage from "../assets/myimage.jfif";
 
+const navLinks = [
+  { name: "home", path: "/" },
+  { name: "projects", path: "/projects" },
+  { contact: "contact", path: "/contact" },
+];
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className={styles.imageWrapper}>
+        <img src={myImage} />
+        <p>mirian tsintsadze</p>
+      </div>
+      {navLinks.map((link, ind) => {
+        <li key={ind}>
+          <NavLink
+            style={({ isActive }) => {
+              return {
+                display: "block",
+                margin: "1rem 0",
+                color: isActive ? "red" : "",
+              };
+            }}
+          >
+            {link.name}
+          </NavLink>
+        </li>;
+      })}
+    </header>
+  );
 };
 
 export default Header;
