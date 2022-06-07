4880c8453a9b26c671aed531f79c708d92b27e97
miriani <mirian.tsintsadze1992@gmail.com>
2022-06-07 12:11:43 +0400

    moving logic done

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,19 +7,28 @@ const Hero = () => {
   useEffect(() => {
     const moveDivs = (e) => {
       const allDivs = divRef.current.childNodes;
+      let rotateValue = 13;
+      const x = (e.pageX * 3) / 100;
+      const y = (e.pageY * 3) / 100;
+      allDivs.forEach((element) => {
+        element.style.transform = `translateX(${x}px) translateY(${y}px) rotate(${rotateValue}deg) `;
+        rotateValue += 13;
+      });
     };
     document.addEventListener("mousemove", moveDivs);
   }, []);
   return (
-    <main className={styles.hero} ref={divRef}>
-      <div className={styles.one}></div>
-      <div className={styles.two}></div>
-      <div className={styles.three}></div>
-      <div className={styles.four}></div>
-      <div className={styles.five}></div>
-      <div className={styles.six}></div>
-      <div className={styles.seven}></div>
-      <div className={styles.eight}></div>
+    <main className={styles.hero}>
+      <article ref={divRef} className={styles.divWrapper}>
+        <div className={styles.one}></div>
+        <div className={styles.two}></div>
+        <div className={styles.three}></div>
+        <div className={styles.four}></div>
+        <div className={styles.five}></div>
+        <div className={styles.six}></div>
+        <div className={styles.seven}></div>
+        <div className={styles.eight}></div>
+      </article>
       <h1>Hi, I'm Miriani</h1>
       <div className={styles.address}>
         <FiMapPin />
