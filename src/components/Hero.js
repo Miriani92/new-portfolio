7dc7ca00c0e65701b7cd94f9a95fd67fafa329d4
miriani <mirian.tsintsadze1992@gmail.com>
2022-06-07 11:14:02 +0400

    addig moving logic

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import styles from "./Hero.module.css";
 import { FiMapPin } from "react-icons/fi";
 
 const Hero = () => {
+  const divRef = useRef(null);
+  useEffect(() => {
+    const moveDivs = (e) => {
+      const allDivs = divRef.current.childNodes;
+    };
+    document.addEventListener("mousemove", moveDivs);
+  }, []);
   return (
-    <main className={styles.hero}>
+    <main className={styles.hero} ref={divRef}>
+      <div className={styles.one}></div>
+      <div className={styles.two}></div>
+      <div className={styles.three}></div>
+      <div className={styles.four}></div>
+      <div className={styles.five}></div>
+      <div className={styles.six}></div>
+      <div className={styles.seven}></div>
+      <div className={styles.eight}></div>
       <h1>Hi, I'm Miriani</h1>
       <div className={styles.address}>
         <FiMapPin />
