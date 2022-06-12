531680525d1cb8c5bd7d3559eeec633f67ce8303
miriani <mirian.tsintsadze1992@gmail.com>
2022-06-12 10:18:25 +0400

    project section

---
+++ src/UI/Projectcard.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Projectcard.module.css";
+
+const Projectcard = () => {
+  return <div>Projectcard</div>;
+};
+
+export default Projectcard;
--- src/page/Page.js
+++ src/page/Page.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Expertise from "../components/Expertise";
-
+import Projects from "../components/Projects";
 const Page = () => {
   return (
     <React.Fragment>
       <Hero />
       <Expertise />
+      <Projects />
     </React.Fragment>
   );
 };
