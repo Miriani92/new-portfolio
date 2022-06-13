3eb7602f9cdff583e07b9f5f198f9ceb5b6c3ed5
miriani <mirian.tsintsadze1992@gmail.com>
2022-06-13 10:38:32 +0400

    projects section

--- src/UI/Projectcard.js
+++ src/UI/Projectcard.js
@@ -3,14 +3,20 @@ import styles from "./Projectcard.module.css";
 
 const Projectcard = ({ name, image1, image2, description }) => {
   return (
-    <div className={styles.card}>
-      <div className={styles.discription}>
-        <h3>{name}</h3>
-        <p>{description}</p>
-      </div>
-      <div className={styles.images}>
-        <img src={image1} />
-        <img src={image2} />
+    <div className={styles.cardwrapper}>
+      <div className={styles.card}>
+        <div className={styles.discription}>
+          <h3>{name}</h3>
+          <p>{description}</p>
+        </div>
+        <div className={styles.images}>
+          <div>
+            <img src={image1} />
+          </div>
+          <div>
+            <img src={image2} />
+          </div>
+        </div>
       </div>
     </div>
   );
