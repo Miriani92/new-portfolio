b3a65249bc7d85f69ad6266babf032e72c856c36
miriani <mirian.tsintsadze1992@gmail.com>
2022-06-14 16:42:13 +0400

    adding contact

---
+++ src/components/Contact.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./Contact.module.css";
+
+const Contact = () => {
+  return (
+    <form>
+      <div>
+        <h2>Get in touch</h2>
+        <label>Name</label>
+        <input placeholder="What should I call you"></input>
+        <label>Subject</label>
+        <input placeholder="What should I call you"></input>
+        <label>Email</label>
+        <input placeholder="What should I call you"></input>
+        <label>essage</label>
+        <textarea> placeholder="What should I call you"</textarea>
+      </div>
+    </form>
+  );
+};
+
+export default Contact;
--- src/page/Page.js
+++ src/page/Page.js
@@ -2,12 +2,14 @@ import React from "react";
 import Hero from "../components/Hero";
 import Expertise from "../components/Expertise";
 import Projects from "../components/Projects";
+import Contact from "../components/Contact";
 const Page = () => {
   return (
     <React.Fragment>
       <Hero />
       <Expertise />
       <Projects />
+      <Contact />
     </React.Fragment>
   );
 };
