4495fe2294fdb55c496e751522923a10acf15c24
miriani <mirian.tsintsadze1992@gmail.com>
2022-06-14 21:55:22 +0400

    contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,17 +6,64 @@ const Contact = () => {
     <form className={styles.form}>
       <div className={styles.formwrapper}>
         <h2>Get in touch</h2>
-        <label>Name</label>
-        <input placeholder="What should I call you"></input>
-        <label>Subject</label>
-        <input placeholder="What should I call you"></input>
-        <label>Email</label>
-        <input placeholder="What should I call you"></input>
-        <label>essage</label>
-        <textarea> placeholder="What should I call you"</textarea>
+        <div tabIndex="0">
+          <label>Name</label>
+          <input placeholder="What should I call you"></input>
+        </div>
+        <div>
+          <label>Subject</label>
+          <input placeholder="What should I call you"></input>
+        </div>
+        <div>
+          <label>Email</label>
+          <input placeholder="What should I call you"></input>
+        </div>
+        <div>
+          <label>Message</label>
+          <textarea></textarea>
+        </div>
+        <button>Submit</button>
       </div>
     </form>
   );
 };
 
 export default Contact;
+
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.my_refs = {};
+//     this.focusByID.bind(this);
+//   }
+
+//   focusByID(id) {
+//     let myRef = this.my_refs[id];
+//     if (myRef) {
+//       console.log("focusing on ", id, myRef);
+//       myRef.focus();
+//     }
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <div
+//           id="bigRedDiv"
+//           style={{ height: 200, width: 200, backgroundColor: "red" }}
+//           ref={(input) => (this.my_refs["bigRedDiv"] = input)}
+//           onFocus={() => console.log("FOCUS IS ON BIG RED DIV")}
+//           onClick={() => this.focusByID("bigRedDiv")}
+//         ></div>
+//       </div>
+//     );
+//   }
+// }
+
+// <div
+//   tabIndex="0" //use this attribute
+//   id="bigRedDiv"
+//   style={{ height: 200, width: 200, backgroundColor: "red" }}
+//   ref={(input) => (this.my_refs["bigRedDiv"] = input)}
+//   onFocus={() => console.log("FOCUS IS ON BIG RED DIV")}
+//   onClick={() => this.focusByID("bigRedDiv")}
+// ></div>;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,18 +6,7 @@ import { projects } from "../data/projectData";
 const Projects = () => {
   return (
     <div className={styles.projects}>
-      <h2
-        style={{
-          marginBottom: "2rem",
-          borderBottom: "2px solid rgba(19, 19, 19, 0.74)",
-          padding: ".5rem",
-          color: "rgba(19, 19, 19, 0.74)",
-          fontWeight: 700,
-        }}
-      >
-        Some of my Projcts
-      </h2>
-
+      <h2 className={styles.title}>Some of my Projcts</h2>
       {projects.map((item, ind) => {
         return <Projectcard key={ind} {...item}></Projectcard>;
       })}
