f75621cccdc303c71fedc7ff348548845b1be651
miriani <mirian.tsintsadze1992@gmail.com>
2022-06-18 20:48:31 +0400

    responsive styles

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,15 +8,15 @@ const Contact = () => {
         <h2>Get in touch</h2>
         <div tabIndex="0">
           <label>Name</label>
-          <input placeholder="What should I call you"></input>
+          <input placeholder="What should I call you" required></input>
         </div>
         <div>
           <label>Subject</label>
-          <input placeholder="Subject of Your interest"></input>
+          <input placeholder="Subject of Your interest" required></input>
         </div>
         <div>
           <label>Email Address</label>
-          <input placeholder="Where should I respond?"></input>
+          <input placeholder="Where should I respond?" required></input>
         </div>
         <div>
           <label>Message</label>
@@ -29,41 +29,3 @@ const Contact = () => {
 };
 
 export default Contact;
-
-// class App extends React.Component {
-//   constructor() {
-//     super();
-//     this.my_refs = {};
-//     this.focusByID.bind(this);
-//   }
-
-//   focusByID(id) {
-//     let myRef = this.my_refs[id];
-//     if (myRef) {
-//       console.log("focusing on ", id, myRef);
-//       myRef.focus();
-//     }
-//   }
-//   render() {
-//     return (
-//       <div>
-//         <div
-//           id="bigRedDiv"
-//           style={{ height: 200, width: 200, backgroundColor: "red" }}
-//           ref={(input) => (this.my_refs["bigRedDiv"] = input)}
-//           onFocus={() => console.log("FOCUS IS ON BIG RED DIV")}
-//           onClick={() => this.focusByID("bigRedDiv")}
-//         ></div>
-//       </div>
-//     );
-//   }
-// }
-
-// <div
-//   tabIndex="0" //use this attribute
-//   id="bigRedDiv"
-//   style={{ height: 200, width: 200, backgroundColor: "red" }}
-//   ref={(input) => (this.my_refs["bigRedDiv"] = input)}
-//   onFocus={() => console.log("FOCUS IS ON BIG RED DIV")}
-//   onClick={() => this.focusByID("bigRedDiv")}
-// ></div>;
