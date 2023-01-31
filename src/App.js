cecf5dbdc06b75ecf3addd49f863b2faea2585b8
Miriani92 <mirian.tsintsadze1992@gmail.com>
2023-01-31 17:58:57 +0400

    change bacgroung onscroll

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Hero from "./components/Hero";
 import Expertise from "./components/Expertise";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import { changeBackgroundColor } from "./utils/changeBackgroundColor";
 import Nav from "./UI/Nav";
 
 function App() {
   const [show, setShow] = useState(false);
 
+  window.addEventListener("scroll", changeBackgroundColor);
   useEffect(() => {
     const onScroll = () => {
       const scrollValue = window.scrollY;
