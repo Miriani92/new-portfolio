0e98852e6eb590c7bd6b6a062c7e2c26f6de6c37
Miriani92 <mirian.tsintsadze1992@gmail.com>
2023-03-24 18:28:26 +0400

    toggle sidenive onScroll

--- src/components/SideSocial.js
+++ src/components/SideSocial.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 import styles from "./SideSocial.module.css";
 import { BsLinkedin, BsGithub, BsFacebook } from "react-icons/bs";
 import { socialLinks } from "../data/Links";
 
 export const SideSocial = () => {
+  const [active, setActive] = useState(true);
+  const leftSideRef = useRef(null);
+  const rightSideRef = useRef(null);
+  useEffect(() => {
+    const footerHeight = 130;
+    window.addEventListener("scroll", (event) => {
+      const scrolled = window.scrollY;
+      const pageScrollHeight = document.body.scrollHeight - window.innerHeight;
+      if (pageScrollHeight - footerHeight <= scrolled) {
+        setActive(false);
+      } else {
+        setActive(true);
+      }
+    });
+  }, []);
   return (
     <>
-      <div className={styles.leftsidewrapper}>
+      <div
+        className={`${styles.leftsidewrapper} ${active && styles.active}`}
+        ref={leftSideRef}
+      >
         <div className={styles.sociallinkswrapper}>
           <a
             href={socialLinks.github}
@@ -32,7 +50,10 @@ export const SideSocial = () => {
         </div>
         <div className={styles.leftline}></div>
       </div>
-      <div className={styles.rightsidewrapper}>
+      <div
+        className={`${styles.rightsidewrapper} ${active && styles.active}`}
+        ref={rightSideRef}
+      >
         <div className={styles.gmail}>mirian.tsintsadze1992@gmail.com</div>
         <div className={styles.rightline}></div>
       </div>
